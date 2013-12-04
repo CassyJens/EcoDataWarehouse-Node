d93e8020812209de7fef3f41da48de1ce5335aa7
Cassy Jens <jens.cass@gmail.com>
2013-12-04 14:00:57 -0600

    Resolved conflicts.

--- backbone/collections/workinggroups.js
+++ backbone/collections/workinggroups.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 var app = app || {};
 
 app.WorkingGroups = Backbone.Collection.extend({
 	model: app.WorkingGroup,
-=======
-var WorkingGroups = Backbone.Collection.extend({
-	model: Wg,
->>>>>>> 3aff4a17e672105911bdd80a91007482c18a8b8f
 	url: '/wgs'
 });
--- backbone/models/wg.js
+++ backbone/models/wg.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 var app = app || {};
 
-app.WorkingGroup = Backbone.Model.extend({	
-=======
-var Wg = Backbone.Model.extend({	
->>>>>>> 3aff4a17e672105911bdd80a91007482c18a8b8f
+app.WorkingGroup = Backbone.Model.extend({
 	idAttribute: '_id',
 	defaults: {
 		name: 'Working Group',
